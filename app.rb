2f40c87bde22262cba77adc355dc3b117f59bf88
Apprentice <apprentice@dbc18.local>
2013-10-01 17:35:22 -0700

    DATABASE IS WORKING AND POPULATED! RAKE TASK FOR SEEDING WINERIES

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/activerecord'  # required by gem sinatra/activerecord
 
 require './user' 
+require './winery' 
 set :database, 'postgres://localhost/wineanddash'
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131001221249) do
+ActiveRecord::Schema.define(version: 20131002002758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20131001221249) do
     t.string   "email"
     t.string   "username"
     t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "wineries", force: true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
